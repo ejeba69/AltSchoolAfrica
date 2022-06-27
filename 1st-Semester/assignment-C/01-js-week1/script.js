d80ec18240fa6a31168fa9ef99425bc00c68358e
Emmanuel Eni <eejeba@gmail.com>
2022-06-27 11:33:00 +0100

    My practice as well as Altschool's resource.

---
+++ 1st-Semester/assignment-C/01-js-week1/script.js
@@ -0,0 +1,20 @@
+// 19:37 sat jun18 wk24 2022
+// This is me trying out my hands at the AltSchool JavaScript week 1 Project.
+
+// let username = prompt(
+//   `Hey there! Welcome to AltSchool Africa! \nPlease enter your Username:`
+// );
+
+// while (username.length > 9)
+//   prompt(`Hey, please enter a username that is less than ten characters:`);
+
+// let password = prompt(`Please enter your password below (password must be more than 6 characters):`)
+
+// while (password.length <= 6) prompt(`Aabaa! Your password must be more than six characters na!`)
+
+let userList = [];
+
+userList[0] = `Samuel`;
+userList[1] = `Michael`;
+
+console.log(userList);
